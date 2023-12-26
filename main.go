9c5503fb48eff41b96678b8f583a8068deb60e05
Madhu <122654918+madhupavan-oss@users.noreply.github.com>
2023-12-26 15:36:21 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	// Type go get -u github.com/gorilla/mux to install
-	"github.com/gorilla/mux" // Unused packages will create compilation error
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Item struct {
@@ -17,7 +21,26 @@ type Item struct {
 	Price float64 `json:"Price"`
 }
 
-var inventory []Item
+var collection *mongo.Collection
+
+func init() {
+	// Connect to MongoDB
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set up the "inventory" collection
+	collection = client.Database("myDB").Collection("inventory")
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Function Called: homePage()")
@@ -27,7 +50,23 @@ func getInventory(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Println("Function Called: getInventory()")
 
-	json.NewEncoder(w).Encode(inventory)
+	// Retrieve items from MongoDB
+	cursor, err := collection.Find(context.Background(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(context.Background())
+
+	var items []Item
+	for cursor.Next(context.Background()) {
+		var item Item
+		if err := cursor.Decode(&item); err != nil {
+			log.Fatal(err)
+		}
+		items = append(items, item)
+	}
+
+	json.NewEncoder(w).Encode(items)
 }
 
 func createItem(w http.ResponseWriter, r *http.Request) {
@@ -36,48 +75,66 @@ func createItem(w http.ResponseWriter, r *http.Request) {
 	var item Item
 	_ = json.NewDecoder(r.Body).Decode(&item) // Obtain item from request JSON
 
-	inventory = append(inventory, item) // Add item to inventory
+	// Insert item into MongoDB
+	_, err := collection.InsertOne(context.Background(), item)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	json.NewEncoder(w).Encode(item) // Show item in response JSON for verification
 }
 
+// The deleteItem, _deleteItemAtUid, and updateItem functions will need modifications to work with MongoDB.
 func deleteItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
+	uid := params["uid"]
 
-	_deleteItemAtUid(params["uid"])
-
-	json.NewEncoder(w).Encode(inventory)
-}
-
-func _deleteItemAtUid(uid string) {
-	for index, item := range inventory {
-		if item.UID == uid {
-			// Delete item from Slice
-			inventory = append(inventory[:index], inventory[index+1:]...)
-			break
-		}
+	result, err := collection.DeleteOne(context.Background(), bson.M{"UID": uid})
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
+
+	if result.DeletedCount == 0 {
+		http.Error(w, "Item not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "Item deleted successfully"})
 }
 
 func updateItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var item Item
-	_ = json.NewDecoder(r.Body).Decode(&item) // Obtain item from request JSON
-
 	params := mux.Vars(r)
+	uid := params["uid"]
 
-	_deleteItemAtUid(params["uid"])     // Delete item
-	inventory = append(inventory, item) // Create it again with data from request
+	var updatedItem Item
+	_ = json.NewDecoder(r.Body).Decode(&updatedItem)
 
-	json.NewEncoder(w).Encode(inventory)
+	result, err := collection.UpdateOne(
+		context.Background(),
+		bson.M{"UID": uid},
+		bson.M{"$set": updatedItem},
+	)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	if result.ModifiedCount == 0 {
+		http.Error(w, "Item not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "Item updated successfully"})
 }
 
 func handleRequests() {
-	// := is the short variable declaration operator
-	// Automatically determines type for variable
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homePage).Methods("GET")
@@ -90,13 +147,5 @@ func handleRequests() {
 }
 
 func main() {
-	// Data store
-	inventory = append(inventory, Item{
-		UID:   "0",
-		Name:  "Cheese",
-		Desc:  "A fine block of cheese.",
-		Price: 4.99,
-	})
-
 	handleRequests()
 }
